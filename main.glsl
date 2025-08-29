7ec109cd11e68a8ed78a208956fbf69cae6d4412
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2025-08-29 21:33:37 +0800

    working reflections

--- main.glsl
+++ main.glsl
@@ -1,42 +1,92 @@
-#include "./structs.glsl"
-
 uniform vec2 u_winsize;
 
-const int MAX = 10000;
-const float STEP = 0.001;
-const float TOL = 0.001;
+#define MAX 5
+#define TOL 1e-3
 
-const float PI = 3.14159265359;
+struct Ray {
+  vec3 position;
+  vec3 direction;
+};
+struct Sphere {
+  vec3 position;
+  vec4 color;
 
-PointLight light = PointLight(vec3(0., 0., 3.), 100., vec4(1.));
-Sphere sphere = Sphere(vec3(0.), 0.5, Mat(vec4(1.)));
-Plane plane = Plane(vec3(0., 0., -1.), vec2(1.), Mat(vec4(1.)));
+  float radius;
+};
+struct Plane {
+  vec3 position;
+  vec4 color;
 
-vec4 traverse(Ray ray, vec4 albedo) {
-  for (int i = 0; i < MAX; i++) {
-    ray.position += ray.direction * STEP;
+  vec3 normal;
+};
+struct Bulb {
+  vec3 position;
+  vec4 color;
 
-    if (distance(ray.position, sphere.center) - sphere.radius < TOL) {
-      vec3 normal = normalize(ray.position - sphere.center);
-      vec3 to_light = light.position - ray.position;
-      ray.direction = reflect(ray.direction, normal);
-      albedo = sphere.material.albedo;
-      float shaded = dot(normalize(ray.direction), normalize(to_light));
-      float power = light.strength / pow(length(to_light), 2.) / 4. / PI;
-      return shaded * power * albedo;
-    }
-  }
+  float strength;
+};
 
-  return albedo;
+Bulb b = Bulb(vec3(1., 0., 3.), vec4(1.), 10.);
+
+float shade(Ray r) {
+  return dot(r.direction, normalize(b.position - r.position));
+}
+
+vec4 intersect(inout Ray r, Sphere s) {
+  // ray: p = p0 + d * t
+  // sphere: dot(p - O, p - O) = r * r
+  // dot(p0 + d * t - O, p0 + d * t - O) - r * r = 0
+  // t^2 * dot(d,d) + 2 * t * dot(d, p0 - O) + dot(p0 - O, p0 - O) - r^2 = 0
+
+  vec3 p = r.position - s.position;
+  float a = dot(r.direction, r.direction);
+  float b = dot(r.direction, p) * 2.;
+  float c = dot(p, p) - pow(s.radius, 2.);
+
+  float det = b * b - 4. * a * c;
+  if (det <= TOL)
+    return vec4(0.);
+
+  float t = (-b + sqrt(det)) / 2. / a;
+  t = min(t, (-b - sqrt(det)) / 2. / a);
+  if (t <= TOL)
+    return vec4(0.);
+
+  r.position += r.direction * t;
+  r.direction = reflect(r.direction, normalize(r.position - s.position));
+  return s.color * shade(r);
+}
+
+vec4 intersect(inout Ray r, Plane p) {
+  // ray: p = p0 + d * t
+  // plane: dot(p - O, N) = 0
+  // dot(p0 + d * t - O, N) = 0
+  // dot(p0 - O, N) + t * dot(d, N) = 0
+
+  float t = dot(p.position - r.position, p.normal) / dot(r.direction, p.normal);
+  if (t <= TOL)
+    return vec4(0.);
+
+  r.position += r.direction * t;
+  r.direction = reflect(r.direction, normalize(p.normal));
+  return p.color * shade(r);
 }
 
 void main() {
-  vec3 start = vec3(0., -5., 0.);
-  vec2 coord = (gl_FragCoord.xy - u_winsize * vec2(0.5));
-  coord /= min(u_winsize.x, u_winsize.y);
-
+  vec3 origin = vec3(0., -5., 0.);
+  vec2 coord =
+      (gl_FragCoord.xy - u_winsize / 2.) / min(u_winsize.x, u_winsize.y);
   vec3 dir = vec3(coord.x, 1., coord.y);
-  Ray ray = Ray(start, dir);
+  Ray ray = Ray(origin, dir);
 
-  gl_FragColor = traverse(ray, vec4(0.));
+  Sphere sphere = Sphere(vec3(-1., 0., 0.), vec4(1.), 0.5);
+  Plane plane = Plane(vec3(0.5, 0., -1.), vec4(1.), vec3(1., 0., 0.));
+
+  vec4 col = vec4(0.);
+  for (int hitN = MAX; hitN > 0; hitN--) {
+    col += intersect(ray, sphere) * hitN;
+    col += intersect(ray, plane) * hitN;
+  }
+  gl_FragColor = col / MAX;
+  // gl_FragColor = vec4(shade(ray));
 }
